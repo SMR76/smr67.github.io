5360c1170d4ce5dd22de265880c712306cda7306
SMR <seyyedmortezarazavi76@gmail.com>
2021-06-26 05:00:57 +0430

    some minor changes.

--- libs/hamberger-button.js
+++ libs/hamberger-button.js
@@ -1,10 +1,10 @@
 $(document).ready(() => {
-  $('#toggle').on('click', function () {
-    $this = $(this);
-    if (!$this.hasClass('on')) {
-      $this.addClass('on');
-    } else {
-      $this.removeClass('on');
-    }
-  });
+	$('#toggle').on('click', function () {
+		$this = $(this);
+		if (!$this.hasClass('on')) {
+			$this.addClass('on');
+		} else {
+			$this.removeClass('on');
+		}
+	});
 });

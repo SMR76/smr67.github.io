ba400e90a20fcc04ba6a446f8202104fd76e2dfd
SMR <seyyedmortezarazavi76@gmail.com>
2021-08-24 01:15:15 +0430

    fixed CORS issue.

--- functions/repositoryDataAjax.php
+++ functions/repositoryDataAjax.php
@@ -8,8 +8,10 @@
  * return JSON data of repositories in database.
  */
 
+header('Access-Control-Allow-Origin: *');
+
 //* include repository data api.
-include_once("repositoryDataHandler.php");
+include_once("repositoryData.php");
 
 $repoDataHandler = new repositoryDataHandler();
 echo $repoDataHandler->getRpositoriesAsJson();
